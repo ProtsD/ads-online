1b5ea57a7572399592f2412846d270f2d72820c1
RUPROTSD <protsdv@gmail.com>
2024-10-17 22:02:12 +0300

    Initial commit

---
+++ src/main/java/ru/ads_online/AdsOnlineApplication.java
@@ -0,0 +1,13 @@
+package ru.ads_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsOnlineApplication.class, args);
+	}
+
+}
