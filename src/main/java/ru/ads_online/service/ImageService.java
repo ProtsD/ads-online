1815da3fac6c198a9103d35252622cd88289427d
RUPROTSD <protsdv@gmail.com>
2025-07-31 00:51:58 +0300

    Add Javadoc to ImageService

--- src/main/java/ru/ads_online/service/ImageService.java
+++ src/main/java/ru/ads_online/service/ImageService.java
@@ -5,14 +5,35 @@ import ru.ads_online.pojo.entity.ImageEntity;
 public interface ImageService {
     String IMAGE_URL_PREFIX = "/images/";
 
-
+    /**
+     * Returns the image entity by its ID.
+     *
+     * @param id the ID of the requested image
+     * @return the image entity
+     */
     ImageEntity getImage(int id);
 
+    /**
+     * Saves a new image.
+     *
+     * @param image the image bytes
+     * @return the saved image entity
+     */
     ImageEntity uploadImage(byte[] image);
 
-
+    /**
+     * Updates the existing image with the specified ID.
+     *
+     * @param id    the ID of the image to update
+     * @param image the new image bytes
+     * @return the updated image entity
+     */
     ImageEntity updateImage(int id, byte[] image);
 
-
+    /**
+     * Deletes the image with the specified ID.
+     *
+     * @param id the ID of the image to delete
+     */
     void deleteImage(int id);
 }
